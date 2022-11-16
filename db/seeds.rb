6bec1c012b7083f907b118646a23bfbade59b3ac
(Ben Kaefer)benkaefer@icloud.com <benkaefer@icloud.com>
2022-11-16 15:59:25 +0000

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create([{name: "mc donalds", address: "london" ,phone_number: "1", category: "fast food"}, {name: "burger king", address: "london" ,phone_number: "2", category: "fast food"}, {name: "honest", address: "london" ,phone_number: "1", category: "fast food"}, {name: "kfc", address: "london" ,phone_number: "1", category: "fast food"}])
